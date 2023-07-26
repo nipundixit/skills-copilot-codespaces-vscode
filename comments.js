32729d3f405fac6dd7b4186ed5fdf48c702ce6bd
nipundixit <82598051+nipundixit@users.noreply.github.com>
2023-07-26 15:59:41 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,28 +1,19 @@
-//create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// Create web server
+// Load the required modules
+const http = require('http');
 
-//set up database
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/comments');
-var commentSchema = new mongoose.Schema({
-    name: String,
-    comment: String
+// Define the host and port for the server
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create the server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
 });
-var Comment = mongoose.model('Comment', commentSchema);
 
-//set up template engine
-app.set('view engine', 'ejs');
-
-//static files
-app.use(express.static('./public'));
-
-//fire controller
-var commentController = require('./controllers/commentController');
-commentController(app);
-
-//listen to port
-app.listen(3000);
-console.log('listening to port 3000');
+// Start the server and listen on the specified port and hostname
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
