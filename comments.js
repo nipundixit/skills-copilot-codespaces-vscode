01655a29d79a52468ba79fbc9fc3bfaeb13601d5
nipundixit <82598051+nipundixit@users.noreply.github.com>
2023-07-26 15:56:25 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,5 +1,28 @@
 //create web server
-import express from 'express';
-const app = express();
-const port = 3000;
-import path from 'path';
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+//set up database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/comments');
+var commentSchema = new mongoose.Schema({
+    name: String,
+    comment: String
+});
+var Comment = mongoose.model('Comment', commentSchema);
+
+//set up template engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+//fire controller
+var commentController = require('./controllers/commentController');
+commentController(app);
+
+//listen to port
+app.listen(3000);
+console.log('listening to port 3000');
